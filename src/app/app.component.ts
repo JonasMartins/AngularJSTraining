8b0282b4aa6c8a18e08e6f74d61a048b73d77024
JonasMartins <jonas.s.martins@hotmail.com>
2017-05-15 21:09:57 -0300

    010-RC5 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+/* Como adicionar metadata a uma classe, metadata são informações adicionais */
 @Component({
-  
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+/* ROOT COMPONENT */
 export class AppComponent {
   title = 'Change This Dynamically!';
 }
