71a67c838cdcf144ed76cc02b2d87946fae3dd6b
JonasMartins <jonas.s.martins@hotmail.com>
2017-05-20 20:45:15 -0300

    021

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+
+  stringInterpolation = 'This is a string interpolation';
+  numberInterpolation = 2;
+  
+  onTest(){
+    return true;
+  }
+}
