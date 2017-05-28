64202a090c1a86e7fb9b1f41bbd4e4053fd707e3
JonasMartins <jonas.s.martins@hotmail.com>
2017-05-28 17:53:03 -0300

    049

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Dummy', 'Dummy', 'https://camo.githubusercontent.com/9e39276ad39fe3cda7ac61dd0f1560dc5ad1ab95/68747470733a2f2f646c2e64726f70626f7875736572636f6e74656e742e636f6d2f752f3737343835392f4769744875622d5265706f732f7465737464756d6d792f63726173687465737464756d6d792e6a7067',[]),
+    new Recipe('Dummy2', 'Dummy2', 'https://camo.githubusercontent.com/9e39276ad39fe3cda7ac61dd0f1560dc5ad1ab95/68747470733a2f2f646c2e64726f70626f7875736572636f6e74656e742e636f6d2f752f3737343835392f4769744875622d5265706f732f7465737464756d6d792f63726173687465737464756d6d792e6a7067',[])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  recipe = new Recipe('Dummy', 'Dummy', 'https://camo.githubusercontent.com/9e39276ad39fe3cda7ac61dd0f1560dc5ad1ab95/68747470733a2f2f646c2e64726f70626f7875736572636f6e74656e742e636f6d2f752f3737343835392f4769744875622d5265706f732f7465737464756d6d792f63726173687465737464756d6d792e6a7067')
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,7 +1,8 @@
+import { Ingredient } from "../shared/ingredient";
 export class Recipe {
 
 /* comando para criar classes: ng g cl recipe*/
-  constructor(public name, public description, public imagePath){
+  constructor(public name, public description, public imagePath, public ingredients: Ingredient[]){
     
   }
 }
