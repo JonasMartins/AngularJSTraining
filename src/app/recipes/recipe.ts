d094e4e3dd71ce8d7aa67fa060341b13cf0b05dc
JonasMartins <jonas.s.martins@hotmail.com>
2017-06-01 21:24:07 -0300

    065

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -2,7 +2,7 @@ import { Ingredient } from "../shared/ingredient";
 export class Recipe {
 
 /* comando para criar classes: ng g cl recipe*/
-  constructor(public name, public description, public imagePath, public ingredients: Ingredient[]){
+  constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]){
     
   }
 }
